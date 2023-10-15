066d41cd633ad9c66634c19a2bca1a4a371bc86d
Wicaksana Pratama <wicaksanapratama@gmail.com>
2023-10-15 22:49:31 +0700

    fix: incorrect shasum

--- Formula/tuan.rb
+++ Formula/tuan.rb
@@ -2,7 +2,7 @@ class Tuan < Formula
   desc "CLI Program to help manage env"
   homepage "https://github.com/wypratama/tuan-cli"
   url "https://github.com/wypratama/tuan-cli/releases/download/v0.1.0/tuan-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "bd661a4b8577ce613fe55fdbe336677a56ba3141"
+  sha256 "0a7fe516a1df313d4c1a9811ae878789a8efc4608801f4735e4bc020e2c9077c"
   version "0.1.0"
 
   def install
